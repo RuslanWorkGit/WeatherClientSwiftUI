62aef7f2dd0f91e8543fff0f346a2143722c93ce
RuslanWorkGit <lulka758@gmail.com>
2025-01-28 20:34:36 +0200

    create empty project

---
+++ WeatherClientSwiftUI/WeatherClientSwiftUI/WeatherClientSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherClientSwiftUIApp.swift
+//  WeatherClientSwiftUI
+//
+//  Created by Ruslan Liulka on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherClientSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
